7a62e27bcec1a80f2d1e104420cf6f80ac0ab08e
Chris Witteveen <Chriswitteveen2@hotmail.com>
2017-02-27 15:16:54 +0100

    Fixed type in Adtje and added routes dor Adtjes

--- app/Adtje.php
+++ app/Adtje.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Adje extends Model
+class Adtje extends Model
 {
     //
 }
--- database/migrations/2017_02_27_141558_create_adtjes_table.php
+++ database/migrations/2017_02_27_141558_create_adtjes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAdjesTable extends Migration
+class CreateAdtjesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateAdjesTable extends Migration
      */
     public function up()
     {
-        Schema::create('adjes', function (Blueprint $table) {
+        Schema::create('adtjes', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreateAdjesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('adjes');
+        Schema::dropIfExists('adtjes');
     }
 }
