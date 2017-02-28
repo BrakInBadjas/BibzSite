75521745a4da69c08ae3e8e232789637a9ce9749
Chris Witteveen <Chriswitteveen2@hotmail.com>
2017-02-28 02:17:58 +0100

    Added succes message for adding adtjes

--- resources/views/adtjes/adtjes.blade.php
+++ resources/views/adtjes/adtjes.blade.php
@@ -16,6 +16,16 @@
                 </div>
             </div>
         @endif
+        @if (Session::has('added_adtje'))
+            <div class="panel panel-success">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Adtje succesvol toegevoegd voor {{ Session::get('added_adtje_for') }}</h3>
+                </div>
+                <div class="panel-body">
+                    {{ Session::get('added_adtje') }}
+                </div>
+            </div>
+        @endif
         <div class="row">
             <div class="col-md-8">
                 <div class="timeline-centered">
