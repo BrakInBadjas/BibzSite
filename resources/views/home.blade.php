9c7429421baeeeaea9946893bc2359562e10e22c
Milo Cesar <milocesar1@gmail.com>
2017-02-28 23:00:59 +0100

    Properly dutching now

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,9 +7,9 @@
             <div class="jumbotron">
                 <h1>
                     @if(($hour = Carbon::now()->hour) < 6)Ga eens slapen!
-                    @elseif($hour < 12)Goede morgen {{Auth::user()->name}}!
-                    @elseif($hour < 18)Goede middag {{Auth::user()->name}}!
-                    @else Goede avond {{Auth::user()->name}}!
+                    @elseif($hour < 12)Goedemorgen {{Auth::user()->name}}!
+                    @elseif($hour < 18)Goedemiddag {{Auth::user()->name}}!
+                    @else Goedenavond {{Auth::user()->name}}!
                     @endif
                 </h1>
             </div>
